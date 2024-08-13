bcbd6081fd938e790b2ff10bbfdef958e12f88ba
John LapTev <152638619+John-LapTev@users.noreply.github.com>
2024-08-13 11:21:06 +0300

    Update22 script.js

--- script.js
+++ script.js
@@ -234,6 +234,14 @@ const levels = [
          [RED, GREEN, GREEN, EMPTY, RED, EMPTY],
          [EMPTY, EMPTY, RED, GREEN, GREEN, RED],
          [GREEN, GREEN, RED, EMPTY, EMPTY, RED]
+    ],
+    [
+         [EMPTY, GREEN, GREEN, RED, GREEN, GREEN],
+         [RED, RED, EMPTY, RED, RED, EMPTY],
+         [RED, RED, KEY, KEY, RED, EMPTY],
+         [RED, GREEN, GREEN, EMPTY, RED, EMPTY],
+         [EMPTY, EMPTY, RED, GREEN, GREEN, GREEN],
+         [EMPTY, EMPTY, RED, EMPTY, EMPTY, EMPTY]
     ],    
 ];
 
